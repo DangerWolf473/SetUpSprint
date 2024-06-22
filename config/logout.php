6feafe1999c76eaa2e353d446ef4c8e0c3d9ab5f
Wardet Wahaj <115442894+DangerWolf473@users.noreply.github.com>
2024-06-23 04:59:39 +0500

    Add files via upload

--- config/logout.php
+++ config/logout.php
@@ -1,7 +1,20 @@
 <?php 
 session_start();
 
-if (isset($_SESSION["userFname"])){
+if (isset($_SESSION["userFname"])) {
+    // Unset all of the session variables
+    $_SESSION = [];
+
+    // Destroy the session
     session_destroy();
-    header("Location:../homePage.php");
+
+    // Redirect to the home page
+    header("Location: ../homePage.php");
+    exit(); // Ensure that no further code is executed after redirection
+} else {
+    // If session variable "userFname" is not set, handle the situation accordingly
+    // For example, redirect to an error page or handle the logic as per your application's requirements
+    header("Location: ../homePage.php"); // Redirect to home page if session variable is not set
+    exit();
 }
+?>
